d62e18efa35c808223ab37cfc8fd8b370b6cdcba
henriquefugie <henriquefugie@gmail.com>
2023-12-04 23:39:53 -0300

    Trabalho 3 pronto

--- Trabalho 3/main.py
+++ Trabalho 3/main.py
@@ -1,7 +1,11 @@
+#Trabalho 3 PAA
+
 import random
 import math
 
 def cruzada(vetor, inicio, meio, fim):
+    
+    # 
     max_esq = max_dir = float('-inf')
     max_atual = 0
     inicio_cruzamento = fim_cruzamento = meio
@@ -26,6 +30,12 @@ def cruzada(vetor, inicio, meio, fim):
     return aux
 
 def maiorSubArrayRecursivo(vetor, inicio, fim):
+    
+    # Esta funcao é a principal do código, onde ele possui primeiramente um caso base, que é quando o inicio é igual ao fim e quando isso acontece, ele vai passa pela esquerda
+    # e vai para a segunda chamada recursiva direita.
+    # Nesta chamada recursiva da esquerda primeiramente, ela separa o array ao meio e depois separa ao meio denovo, resolvendo um problema menor para ir proseguindo com as demais solucoes,
+    # ele vai repetindo esse processo de encontra esquerda, encontra direita, faz o cruzamento até alcancar o objetivo final que é quando o cruzamento recebe o array inteiro, mesmo não fazendo o cruzamento
+    # considerando o vetor inteiro.
     if inicio == fim:
         return vetor[inicio:inicio + 1]
 
@@ -38,6 +48,9 @@ def maiorSubArrayRecursivo(vetor, inicio, fim):
     return cadeia
 
 def verifica(esq, meio, direita):
+    
+    # Esta parte do código que retornar basicamente qual das tres cadeias encontradas, tanto pela esquerda, quando pela direita e pelo meio, qual delas possui o maior valor
+    # de soma, e o que for escolhido é retornado, e assim em diante, a cada recursão feita, onde os valores de esquerda, direita e cruzamento foram encontrado
     s1 = sum(esq)
     s2 = sum(direita)
     s3 = sum(meio)
@@ -49,6 +62,7 @@ def verifica(esq, meio, direita):
     else:
         return meio
 
+# Este código comeca deste ponto onde ele mostra um menu se o usuario deseja receber um array aleatorio de 10 posicoes, ou digitar um array proprio
 print("1. Array Aleatorio")
 print("2. Array Digitado")
 escolha = input("Escolha uma opção: ")
@@ -56,12 +70,13 @@ escolha = input("Escolha uma opção: ")
 if escolha == "1":
     array = [random.randint(-100, 100) for _ in range(10)]
 elif escolha == "2":
-    array_digitado = input("Digite o array: ")
+    array_digitado = input("Digite o array - Ex(1,2,3,4,5,6):")
     array = [int(numero) for numero in array_digitado.split(',')]
 
+# O array é mostrado para fins de confirmacao, para garantir que o array foi digitado corretamente
 print(array)
 
-# Chama a função recursiva para encontrar a maior soma em subvetores
+# Chama a função recursiva para encontrar a maior soma em subvetores e o -1 é porque o array comeca do 0, ai o len encontra 10, sendo que o array vai até 9
 subvetor_max = maiorSubArrayRecursivo(array, 0, len(array) - 1)
 soma_max = sum(subvetor_max)
 
